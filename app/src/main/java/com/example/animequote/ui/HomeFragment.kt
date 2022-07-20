ea55bd420919c592625ddf4bb8e2d6ecb5dc1094
Nassim Errahoui <nerrahoui@ippon.fr>
2022-07-20 20:14:39 +0200

    Add binding in home fragment

--- app/src/main/java/com/example/animequote/ui/HomeFragment.kt
+++ app/src/main/java/com/example/animequote/ui/HomeFragment.kt
@@ -8,27 +8,29 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.example.animequote.R
+import com.example.animequote.databinding.FragmentHomeBinding
 import com.example.animequote.ui.viewmodel.AvailableAnimeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     private val availableAnimeViewModel: AvailableAnimeViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         availableAnimeViewModel.loadAvailableAnime()
         view.findViewById<RecyclerView>(R.id.anime_title_recycler_view).apply {
-            // todo
+            println("TOTO " + availableAnimeViewModel.availableAnime)
         }
-
     }
 }
