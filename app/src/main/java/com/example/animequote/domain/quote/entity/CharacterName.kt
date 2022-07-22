77e381593e35a8ff32c32ecd3c22de1ec724d057
Nassim Errahoui <nerrahoui@ippon.fr>
2022-07-22 22:58:17 +0200

    Remove private visibility on value class

--- app/src/main/java/com/example/animequote/domain/quote/entity/CharacterName.kt
+++ app/src/main/java/com/example/animequote/domain/quote/entity/CharacterName.kt
@@ -1,7 +1,7 @@
 package com.example.animequote.domain.quote.entity
 
 @JvmInline
-value class CharacterName(private val value: String) {
+value class CharacterName(val value: String) {
     init {
         if (value.isEmpty()) {
             throw IllegalArgumentException("value can't be empty")
--- app/src/main/java/com/example/animequote/domain/quote/entity/QuoteValue.kt
+++ app/src/main/java/com/example/animequote/domain/quote/entity/QuoteValue.kt
@@ -1,7 +1,7 @@
 package com.example.animequote.domain.quote.entity
 
 @JvmInline
-value class QuoteValue(private val value: String) {
+value class QuoteValue(val value: String) {
     init {
         if (value.isEmpty()) {
             throw IllegalArgumentException("value can't be empty")
