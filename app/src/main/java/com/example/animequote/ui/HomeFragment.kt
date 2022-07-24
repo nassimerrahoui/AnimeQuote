766807ea39fc73351dad1b7a4a306e7a014de604
Nassim Errahoui <nerrahoui@ippon.fr>
2022-07-24 10:33:09 +0200

    Add card view and item decorator for available anime list

--- app/src/main/java/com/example/animequote/ui/HomeFragment.kt
+++ app/src/main/java/com/example/animequote/ui/HomeFragment.kt
@@ -7,9 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.animequote.R
 import com.example.animequote.databinding.FragmentHomeBinding
 import com.example.animequote.domain.quote.entity.AnimeTitle
 import com.example.animequote.ui.adapter.AnimeTitleAdapter
+import com.example.animequote.ui.decorator.AnimeTitleItemDecorator
 import com.example.animequote.ui.viewmodel.AvailableAnimeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -32,6 +35,8 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.animeTitleRecyclerView.apply {
             adapter = animeTitleAdapter
+            layoutManager = LinearLayoutManager(context)
+            this.addItemDecoration(AnimeTitleItemDecorator(resources.getDimensionPixelSize(R.dimen.card_margin)))
         }
         availableAnimeViewModel.loadAvailableAnime()
         availableAnimeViewModel.availableAnime.observe(viewLifecycleOwner, onNewAnimeTitle)
